d0b8f46ce2830fd6a7f468a3eb6945a50b879163
Danny Grove <danny@drgrovellc.com>
2017-01-22 22:52:17 -0800

    profile.d/nvm.sh: add support for nvm

---
+++ profile.d/nvm.sh
@@ -0,0 +1,17 @@
+export NVM_DIR="$HOME/.nvm"
+
+install_nvm() {
+  echo "Installing NVM"
+  git clone https://github.com/creationix/nvm "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+  . "$NVM_DIR/nvm.sh"
+}
+
+if [ ! -d  "$NVM_DIR" ]
+then
+  install_nvm
+fi
+
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+
