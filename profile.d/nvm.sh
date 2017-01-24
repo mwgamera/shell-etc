5dc80746cb0e05a0a58073b98959950c85957753
Danny Grove <danny@drgrovellc.com>
2017-01-23 18:59:40 -0800

    Update checkout command to use newer iteroplation

--- profile.d/nvm.sh
+++ profile.d/nvm.sh
@@ -3,7 +3,7 @@ export NVM_DIR="$HOME/.nvm"
 install_nvm() {
   git clone https://github.com/creationix/nvm "$NVM_DIR"
   cd "$NVM_DIR"
-  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+  git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" origin)
   . "$NVM_DIR/nvm.sh"
 }
 
